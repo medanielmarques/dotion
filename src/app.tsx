2a56c3b7c2138baf497b76b4d4dac6ffa66d35ec
Daniel Marques <daniel.brz2009@gmail.com>
2022-10-01 20:34:16 -0300

    WIP

--- src/app.tsx
+++ src/app.tsx
@@ -18,7 +18,7 @@ function App() {
     (e: MouseEvent) => {
       e.preventDefault();
       setAnchorPoint({ x: e.pageX, y: e.pageY });
-      setShowContextMenu(true);
+      setShowContextMenu((show) => !show);
     },
     [setAnchorPoint, setShowContextMenu]
   );
@@ -32,52 +32,63 @@ function App() {
   });
 
   return (
-    <div className='mx-auto container p-12 flex flex-col gap-3'>
-      <div className='flex gap-2'>
-        <input
-          type='text'
-          value={input}
-          onChange={(e) => handleInputChange(e.currentTarget.value)}
-          className='rounded-md w-72 text-gray-800 p-2'
-          onKeyDown={addTodoWithEnterKey}
-          placeholder='My super fun task :D'
-        />
+    <>
+      <div className='mx-auto container p-12 flex flex-col gap-3'>
+        <div className='flex gap-2'>
+          <input
+            type='text'
+            value={input}
+            onChange={(e) => handleInputChange(e.currentTarget.value)}
+            className='rounded-md w-72 text-gray-800 p-2'
+            onKeyDown={addTodoWithEnterKey}
+            placeholder='My super fun task :D'
+          />
 
-        <button
-          className='p-2 bg-gray-500 rounded-md hover:bg-gray-400'
-          onClick={() => input !== '' && addTodo()}
-        >
-          add task
-        </button>
-      </div>
-
-      <div>
-        {todos.map((todo, i) => (
-          <div
-            key={i}
-            contextMenu='mymenu'
-            // onContextMenu={(e) => console.log(e)}
-            className='flex gap-2 items-center'
+          <button
+            className='p-2 bg-gray-500 rounded-md hover:bg-gray-400'
+            onClick={() => input !== '' && addTodo()}
           >
-            <input
-              type='checkbox'
-              className='w-5 h-5 cursor-pointer bg-black-900'
-              checked={todo.done}
-              onChange={() => toggleTodo(i)}
-            />
-            <span
-              className='cursor-default text-lg'
-              style={{
-                textDecoration: todo.done ? 'line-through' : '',
-                color: todo.done ? 'gray' : 'inherit',
-              }}
+            add task
+          </button>
+        </div>
+
+        <div>
+          {todos.map((todo, i) => (
+            <div
+              key={i}
+              contextMenu='mymenu'
+              // onContextMenu={(e) => console.log(e)}
+              className='flex gap-2 items-center'
             >
-              {todo.task}
-            </span>
-          </div>
-        ))}
+              <input
+                type='checkbox'
+                className='w-5 h-5 cursor-pointer bg-black-900'
+                checked={todo.done}
+                onChange={() => toggleTodo(i)}
+              />
+              <span
+                className='cursor-default text-lg'
+                style={{
+                  textDecoration: todo.done ? 'line-through' : '',
+                  color: todo.done ? 'gray' : 'inherit',
+                }}
+              >
+                {todo.task}
+              </span>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+
+      {showContextMenu && (
+        <div
+          className='bg-neutral-300 w-40 h-40 absolute'
+          style={{ top: anchorPoint.y, left: anchorPoint.x }}
+        >
+          <button>stuff</button>
+        </div>
+      )}
+    </>
   );
 }
 
